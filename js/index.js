54392d2fd650831866b940b2b0ab3c6d29593645
QuizairaRodriguez <kiss02andre@gmail.com>
2022-09-08 13:52:42 -0300

    style_index.css creation and others modifications

---
+++ js/index.js
@@ -0,0 +1,5 @@
+let moon = document.querySelector('#moon');
+let horizon = document.querySelector('#horizon');
+let welcome = document.querySelector('#welcom');
+let rocks = document.querySelector('#rocks');
+let woman = document.querySelector('#woman');
