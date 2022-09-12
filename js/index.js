5c6ca0bd859226b6bb883031501067147c8f5bd3
QuizairaRodriguez <kiss02andre@gmail.com>
2022-09-12 16:48:09 -0300

    menu and footer responsive

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ let horizon = document.querySelector('#horizon');
 let welcome = document.querySelector('.welcome');
 let rocks = document.querySelector('#rocks');
 let woman = document.querySelector('#woman');
+let navToggle = document.querySelector(".nav-toggle");
+let navMenu = document.querySelector("nav");
 
 window.addEventListener('scroll', function(){
     
@@ -11,3 +13,13 @@ window.addEventListener('scroll', function(){
     moon.style.top = (500+(value/2)) +'px';
     welcome.style.bottom = (100+(value/2)) +'px';
 })
+
+navToggle.addEventListener('click', function(){
+    navMenu.classList.toggle("visible_menu");
+
+    if (navMenu.classList.contains("visible_menu")) {
+        navToggle.setAttribute("aria-label", "Cerrar menú");
+    } else {
+        navToggle.setAttribute("aria-label", "Abrir menú");
+    }
+});
