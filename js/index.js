5bf792415d1a9f211316544f1dfb4ccc104f5c44
QuizairaRodriguez <kiss02andre@gmail.com>
2022-09-11 22:27:31 -0300

    presentation, animations, footer and css

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 let moon = document.querySelector('#moon');
 let horizon = document.querySelector('#horizon');
-let welcome = document.querySelector('#welcom');
+let welcome = document.querySelector('.welcome');
 let rocks = document.querySelector('#rocks');
 let woman = document.querySelector('#woman');
+
+window.addEventListener('scroll', function(){
+    
+    let value = this.scrollY;
+    console.log(value)
+    moon.style.top = (500+(value/2)) +'px';
+    welcome.style.bottom = (100+(value/2)) +'px';
+})
